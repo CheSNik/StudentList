2153b8261fec40d7788864a4ee05eb58185edd00
4esniy <chekhoninsn@gmail.com>
2020-08-13 02:05:35 -0700

    Fisrt commit

---
+++ src/app/students/student.ts
@@ -0,0 +1,10 @@
+export class Student{
+    _id: number;
+    name: String;
+    grades: String[];
+    img: String;
+    gender: String;
+    birthday: String;
+    athlete: boolean;
+    grade: number;
+}
